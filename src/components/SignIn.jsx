8674d2d6c09f7c7ab12fb627d023400b0b0968ec
rajeshkrack <rajeshkumbhakarn979@gmail.com>
2024-12-26 18:31:37 +0530

    try to solve deployment issue

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs"; // Importing Tab components
 import { TabsDemo } from './Tabsdemo'; // Importing another demo component (though not used here)
 import RightComp from './RightComp'; // Importing the right-hand component (sign-in form)
@@ -6,18 +6,33 @@ import LeftComp from './LeftComp'; // Importing the left-hand component (image o
 
 const SignIn = () => {
     return (
-        <div>
-            <div className="h-screen flex">
-                {/* Left Column (Image or other content) */}
-                <div className="flex-1 bg-white">
-                    <LeftComp />  {/* This component is rendered in the left column */}
-                </div>
+        <div className="h-screen flex">
+            {/* Left Column (Image or other content) */}
+            <div className="flex-1 bg-white">
+                <LeftComp />  {/* This component is rendered in the left column */}
+            </div>
 
-                {/* Right Column (Sign-In Form) */}
-                <div className="flex-1 flex items-center justify-center bg-[#FAFAFA]">
-                    <div>
-                        <RightComp />  {/* This component is rendered in the right column */}
-                    </div>
+            {/* Right Column (Sign-In Form) */}
+            <div className="flex-1 flex items-center justify-center bg-[#FAFAFA]">
+                <div className="w-full max-w-md">
+                    {/* Tabs Component Example */}
+                    <Tabs>
+                        <TabsList className="flex border-b">
+                            <TabsTrigger value="signin">Sign In</TabsTrigger>
+                            <TabsTrigger value="signup">Sign Up</TabsTrigger>
+                        </TabsList>
+
+                        {/* Tab Contents */}
+                        <TabsContent value="signin">
+                            <RightComp />  {/* This component is rendered for the 'Sign In' tab */}
+                        </TabsContent>
+                        <TabsContent value="signup">
+                            <div>Sign Up Form Placeholder</div>  {/* Add sign-up form content here */}
+                        </TabsContent>
+                    </Tabs>
+
+                    {/* Optionally, use TabsDemo for further usage */}
+                    <TabsDemo />
                 </div>
             </div>
         </div>
